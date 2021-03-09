8013a9ce4cd66f0eb5b533f096f2bcfecfe4b266
Rafael Farias <rafael21boy@hotmail.com>
2021-03-08 21:59:32 -0300

    Configurando o Apache nas VMs

---
+++ gcp-user-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+sudo apt-get update 
+sudo apt-get install -y apache2 
+sudo systemctl start apache2 
+sudo systemctl enable apache2 
+echo "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'><h1> Lab MultiCloud Google Cloud </h1>" | sudo tee /var/www/html/index.html
